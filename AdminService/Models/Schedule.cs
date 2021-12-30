f15f75513a3a07071bda2ce06d3285987a9e0039
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2021-12-30 17:50:34 +0300

    initial data model

---
+++ AdminService/Models/Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminService.Models {
+    public class Schedule {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public List<TripAppointment> Appointments { get; set; }
+    }
+}
---
+++ AdminService/Models/TripAppointment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminService.Models {
+    public class TripAppointment {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public TripDescription TripDescription { get; set; }
+    }
+}
