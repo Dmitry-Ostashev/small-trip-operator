48720667269c811063f6ab3dafef3b0d3ebe7f86
Dmitry-Ostashev <dmitry.ostashev@devexpress.com>
2022-01-02 22:17:08 +0300

    required fields

--- AdminService/Models/Schedule.cs
+++ AdminService/Models/Schedule.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdminService.Models {
     public class Schedule {
+        [Key, Required]
         public string Id { get; set; }
 
         public string Title { get; set; }
--- AdminService/Models/TripAppointment.cs
+++ AdminService/Models/TripAppointment.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AdminService.Models {
     public class TripAppointment {
+        [Key, Required]
         public int Id { get; set; }
         public decimal Price { get; set; }
         public DateTime Date { get; set; }
